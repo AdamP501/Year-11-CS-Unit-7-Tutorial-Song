5dec51af9ef68e32790cd5a82e23ad7492c815f8
AdamP501 <axonp2026@student.cis.edu.hk>
2024-03-04 14:21:15 +0800

    finished

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+
+    private ArrayList<String> oldListeners = new ArrayList<>();
+
+    public Song(String title, String artist)
+    {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    public int howMany(ArrayList<String> listeners)
+    {
+        int newListeners = 0;
+        for (String listener : listeners)
+        {
+            String caseInsensitive = listener.toLowerCase();
+            if (!oldListeners.contains(caseInsensitive))
+            {
+                oldListeners.add(caseInsensitive);
+                newListeners ++;
+            }
+        }
+        return newListeners;
+    }
+
+
 }
